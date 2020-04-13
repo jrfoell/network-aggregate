a53a627cc3457408bd1c513d23ee45e32bccaa99
Justin Foell <630830+jrfoell@users.noreply.github.com>
2020-04-12 19:51:09 -0500

    Added taxonomy term link fix

--- network-aggregate.php
+++ network-aggregate.php
@@ -12,6 +12,25 @@ class NetworkAggregate {
 	private $db;
 	private $blog_ids;
 
+	/**
+	 * Blog ID for the current site being operated on in "The Loop".
+	 *
+	 * @var int
+	 */
+	private $current_blog;
+
+	/**
+	 * Array of WP_Rewrites per blog ID.
+	 *
+	 * @var array
+	 */
+	private $taxonomy_rewrites = array();
+
+	/**
+	 * Constructor.
+	 *
+	 * @author Justin Foell <justin@foell.org>
+	 */
 	public function __construct() {
 		global $wpdb;
 		$this->db = $wpdb;
@@ -47,11 +66,47 @@ class NetworkAggregate {
 	public function loop_start( $query ) {
 		add_action( 'the_post', array( $this, 'the_post' ) );
 		add_action( 'loop_end', array( $this, 'loop_end' ) );
+		add_filter( 'pre_term_link', array( $this, 'pre_term_link' ), 10, 2 );
+
+	}
+
+	/**
+	 * Make sure per-blog taxonomy term links are correct.
+	 *
+	 * @param string  $termlink Term link for the site (likely formatted for the parent site).
+	 * @param WP_Term $term Term object.
+	 * @return string Permalink structure for a term link.
+	 * @since  1.2
+	 */
+	public function pre_term_link( $termlink, $term ) {
+		if ( BLOG_ID_CURRENT_SITE === $this->current_blog ) {
+			return $termlink;
+		}
+
+		/**
+		 * Keep one set of taxonomy rewrites per blog ID.
+		 */
+		if ( empty( $this->taxonomy_rewrites[ $this->current_blog ] ) ) {
+			$this->taxonomy_rewrites[ $this->current_blog ] = new WP_Rewrite();
+		}
+
+		$wp_rewrite = $this->taxonomy_rewrites[ $this->current_blog ];
+
+		/**
+		 * Add taxonomies to the per-site rewrite rules as we encounter them.
+		 */
+		if ( false === $wp_rewrite->get_extra_permastruct( $term->taxonomy ) ) {
+			$taxonomy = get_taxonomy( $term->taxonomy );
+			$wp_rewrite->add_permastruct( $taxonomy->name, "{$taxonomy->rewrite['slug']}/%$taxonomy->name%", $taxonomy->rewrite );
+		}
+
+		return $wp_rewrite->get_extra_permastruct( $term->taxonomy );
 	}
 
 	public function the_post( $post ) {
 		restore_current_blog(); //reset to center so we remember for loop_end
 		switch_to_blog( $post->blog_id );
+		$this->current_blog = $post->blog_id;
 	}
 
 	public function loop_end( $query ) {
@@ -84,5 +139,7 @@ class NetworkAggregate {
 	}
 }
 
-$na_plugin = new NetworkAggregate();
-add_action( 'init', array( $na_plugin, 'init' ) );
+if ( defined( 'BLOG_ID_CURRENT_SITE' ) ) {
+	$na_plugin = new NetworkAggregate();
+	add_action( 'init', array( $na_plugin, 'init' ) );
+}
